a9157005fc4ce4d7c568862915b9bbb6c4fde068
mardoqueu.telvina <mardoqueu.telvina@cibernix.com>
2024-11-27 14:27:03 -0300

    feat:ready for deploy

--- src/main/java/com/challenge/gateway/config/CorsConfig.java
+++ src/main/java/com/challenge/gateway/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://calculator-front-chi.vercel.app/")
+                .allowedOrigins("https://calculator-front-chi.vercel.app", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
